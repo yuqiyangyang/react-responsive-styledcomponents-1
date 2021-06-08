c861b3f92a60c4644657bea76a2ccf98610bc483
yuqi <yuqi.yang@mail.mcgill.ca>
2021-06-08 16:00:07 -0400

    setup Nav, Router

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
+}
+
+export default App;
